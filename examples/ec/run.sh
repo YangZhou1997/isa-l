07cbb738283f55e9d8e4d635ecd0a091cf636e0b
YangZhou1997 <yangzhou@g.harvard.edu>
2021-05-08 22:55:28 -0600

    using large file size

--- examples/ec/run.sh
+++ examples/ec/run.sh
@@ -1,5 +1,9 @@
 # !/bin/bash
 
-for i in 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152; do
-    ./ec_simple_example -k 3 -p 2 -l 2097152
+echo 20480 | sudo tee /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
+
+for pagesize in 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152; do
+    ./ec_simple_example -k 3 -p 2 -l $pagesize
 done
+
+echo 0 | sudo tee /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
