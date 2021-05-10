f5d3d7d1bd2d963c038ac78ae9e5535664f9d19e
yangzhou <yangzhou@node-0.yangzhou-qv98431.lambda-mpi-pg0.apt.emulab.net>
2021-05-09 23:25:07 -0600

    fixing run.sh

--- examples/ec/run.sh
+++ examples/ec/run.sh
@@ -1,12 +1,14 @@
 # !/bin/bash
 
-echo 20000 | sudo tee /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
+all_ps=(512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 8388608 16777216 33554432 67108864)
+# all_ps=(4194304 8388608 16777216 33554432 67108864)
 
 make clean
 make
+echo 0 | sudo tee /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
 for k in 3 8; do
     all_res=""
-    for pagesize in 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152; do
+    for pagesize in ${all_ps[@]}; do
         output=`taskset -c 0 ./ec_simple_example -k $k -p 2 -l $pagesize`
         echo $output
         number=`echo $output | grep speed | awk '{ print $11; }'`
@@ -17,9 +19,10 @@ done
 
 make clean
 make CFLAGS="-DUSING_HG"
+echo 20000 | sudo tee /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
 for k in 3 8; do
     all_res=""
-    for pagesize in 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152; do
+    for pagesize in ${all_ps[@]}; do
         output=`taskset -c 0 ./ec_simple_example -k $k -p 2 -l $pagesize`
         echo $output
         number=`echo $output | grep speed | awk '{ print $11; }'`
@@ -27,5 +30,4 @@ for k in 3 8; do
     done
     echo "with hugepage RS($k, 2): " $all_res
 done
-
 echo 0 | sudo tee /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
